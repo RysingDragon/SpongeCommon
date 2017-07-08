a2a9871d4b6c888fd4905e643c28c6760624619e
Daniel Naylor <git@drnaylor.co.uk>
2017-07-08 22:49:22 +0100

    Add more impl, mostly around the CommandSpec

--- src/main/java/org/spongepowered/common/command/SpongeCommandManager.java
+++ src/main/java/org/spongepowered/common/command/SpongeCommandManager.java
@@ -136,8 +136,23 @@ public class SpongeCommandManager implements CommandManager {
                             + "(in other words, is 'plugin' actually your plugin object?");
         }
 
-        PluginContainer container = containerOptional.get();
+        return register(containerOptional.get(), callable, aliases);
+    }
 
+    @Override
+    public Optional<CommandMapping> register(PluginContainer pluginContainer, CommandLowLevel callable, String... alias) {
+        return register(pluginContainer, callable, Arrays.asList(alias));
+    }
+
+    @Override
+    public Optional<CommandMapping> register(PluginContainer pluginContainer, CommandLowLevel callable, List<String> aliases) {
+        return register(pluginContainer, callable, aliases, Function.identity());
+    }
+
+    @Override
+    public Optional<CommandMapping> register(PluginContainer pluginContainer, CommandLowLevel callable, List<String> aliases,
+            Function<List<String>, List<String>> callback) {
+        checkNotNull(pluginContainer, "pluginContainer");
         synchronized (this.lock) {
             // <namespace>:<alias> for all commands
             List<String> aliasesWithPrefix = new ArrayList<>(aliases.size() * 3);
@@ -147,7 +162,7 @@ public class SpongeCommandManager implements CommandManager {
                     this.logger.debug("Plugin '{}' is attempting to register duplicate alias '{}'", container.getId(), alias);
                     continue;
                 }
-                final Collection<CommandMapping> ownedCommands = this.owners.get(container);
+                final Collection<CommandMapping> ownedCommands = this.owners.get(pluginContainer);
                 for (CommandMapping mapping : this.dispatcher.getAll(alias)) {
                     if (ownedCommands.contains(mapping)) {
                         boolean isWrapper = callable instanceof MinecraftCommandWrapper;
@@ -158,14 +173,14 @@ public class SpongeCommandManager implements CommandManager {
                 }
 
                 aliasesWithPrefix.add(alias);
-                aliasesWithPrefix.add(container.getId() + ':' + alias);
+                aliasesWithPrefix.add(pluginContainer.getId() + ':' + alias);
             }
 
             Optional<CommandMapping> mapping = this.dispatcher.register(callable, aliasesWithPrefix, callback);
 
             if (mapping.isPresent()) {
-                this.owners.put(container, mapping.get());
-                this.reverseOwners.put(mapping.get(), container);
+                this.owners.put(pluginContainer, mapping.get());
+                this.reverseOwners.put(mapping.get(), pluginContainer);
             }
 
             return mapping;
@@ -287,6 +302,11 @@ public class SpongeCommandManager implements CommandManager {
     }
 
     @Override
+    public Optional<String> getPrimaryAlias(CommandLowLevel command) {
+        return this.reverseOwners.keySet().stream().filter(x -> x.getCallable() == command).findFirst().map(CommandMapping::getPrimaryAlias);
+    }
+
+    @Override
     public CommandExecutionResult process(CommandSource source, String commandLine) {
         final String[] argSplit = commandLine.split(" ", 2);
         Sponge.getCauseStackManager().pushCause(source);
@@ -393,7 +413,7 @@ public class SpongeCommandManager implements CommandManager {
                 src.sendMessage(error(t("Error getting suggestions: %s", ((CommandException) e).getText())));
                 return Collections.emptyList();
             }
-            throw new RuntimeException(String.format("Error occured while tab completing '%s'", arguments), e);
+            throw new RuntimeException(String.format("Error occurred while tab completing '%s'", arguments), e);
         }
     }
 
